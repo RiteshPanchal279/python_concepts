758392fe96f26631dcd5e81f10709e1c5e48e3cd
RiteshPanchal279 <riteshpanchal279@gmail.com>
2025-04-09 12:43:51 +0530

    javascript

---
+++ javascript/datatype.js
@@ -0,0 +1,32 @@
+let one=34;
+let two=99;
+const ans=one+two
+console.log(ans)
+// -------------------------------->
+var first = 121;
+var second = 202;
+
+var add = first+second;
+console.log(`Addition of ${first} and ${second}: `+ add);
+
+var sub=second-first;
+console.log(`Substarction of ${first} and ${second}: `+sub);
+
+var mul = first*second;
+console.log(`Multiplication of ${first} and ${second}: `+mul);
+
+var div=second/first;
+console.log(`Division of ${first} and ${second}: `+div);
+// -------------------------------->
+var name = "User";
+var cname = 'Anudip';
+
+console.log(`Hello ${name} !!  
+Welcome to ${cname}`);
+// -------------------------------->
+let str = new String("hello me")
+console.log(str) // [String: 'hello me']
+console.log(typeof(str))//object
+let consrut = str.constructor
+console.log(consrut)//[Function: String]
+console.log(typeof(consrut))//function
